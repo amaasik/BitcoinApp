30cd630673c6a97021a3f13363168cc3d6642ae9
Anni Maasik <amaasik@gmail.com>
2022-03-09 11:46:41 +0200

    Add project files.

---
+++ BitcoinCalculator/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BitcoinCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.currencyCombo = new System.Windows.Forms.ComboBox();
+            this.btnGetRates = new System.Windows.Forms.Button();
+            this.ammountOfCoinBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // currencyCombo
+            // 
+            this.currencyCombo.FormattingEnabled = true;
+            this.currencyCombo.Items.AddRange(new object[] {
+            "EUR",
+            "USD"});
+            this.currencyCombo.Location = new System.Drawing.Point(106, 48);
+            this.currencyCombo.Name = "currencyCombo";
+            this.currencyCombo.Size = new System.Drawing.Size(151, 28);
+            this.currencyCombo.TabIndex = 0;
+            this.currencyCombo.Text = "Select currency";
+            this.currencyCombo.SelectedIndexChanged += new System.EventHandler(this.Currency_SelectedIndexChanged);
+            // 
+            // btnGetRates
+            // 
+            this.btnGetRates.BackColor = System.Drawing.SystemColors.Info;
+            this.btnGetRates.Location = new System.Drawing.Point(106, 172);
+            this.btnGetRates.Name = "btnGetRates";
+            this.btnGetRates.Size = new System.Drawing.Size(94, 29);
+            this.btnGetRates.TabIndex = 1;
+            this.btnGetRates.Text = "Get rates";
+            this.btnGetRates.UseVisualStyleBackColor = false;
+            this.btnGetRates.Click += new System.EventHandler(this.btnGetRates_Click);
+            // 
+            // ammountOfCoinBox
+            // 
+            this.ammountOfCoinBox.Location = new System.Drawing.Point(121, 122);
+            this.ammountOfCoinBox.Name = "ammountOfCoinBox";
+            this.ammountOfCoinBox.Size = new System.Drawing.Size(125, 27);
+            this.ammountOfCoinBox.TabIndex = 2;
+            this.ammountOfCoinBox.TextChanged += new System.EventHandler(this.ammountOfCoin_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(121, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Amount of Bitcoins";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.BackColor = System.Drawing.Color.Transparent;
+            this.resultLabel.ForeColor = System.Drawing.Color.White;
+            this.resultLabel.Location = new System.Drawing.Point(121, 224);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(49, 20);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "Result";
+            this.resultLabel.Visible = false;
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(121, 247);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(125, 27);
+            this.ResultTextBox.TabIndex = 5;
+            this.ResultTextBox.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(959, 520);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ammountOfCoinBox);
+            this.Controls.Add(this.btnGetRates);
+            this.Controls.Add(this.currencyCombo);
+            this.Name = "Form1";
+            this.Text = "BitcoinCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox currencyCombo;
+        private System.Windows.Forms.Button btnGetRates;
+        private System.Windows.Forms.TextBox ammountOfCoinBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox ResultTextBox;
+    }
+}
---
+++ BitcoinCalculator/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitcoinCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetRates_Click(object sender, EventArgs e)
+        {
+            if(currencyCombo.SelectedItem.ToString() == "EUR")
+            {
+                resultLabel.Visible = true;
+                ResultTextBox.Visible = true;
+                BitcoinRates bitcoin = GetRates();
+                float result = Int32.Parse(ammountOfCoinBox.Text) * bitcoin.bpi.EUR.rate_float;
+                ResultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.EUR.code}";
+                
+                
+            }
+            if (currencyCombo.SelectedItem.ToString() == "USD")
+            {
+                resultLabel.Visible = true;
+                ResultTextBox.Visible = true;
+                BitcoinRates bitcoin = GetRates();
+                float result = Int32.Parse(ammountOfCoinBox.Text) * bitcoin.bpi.USD.rate_float;
+                ResultTextBox.Text = $"{result.ToString()} {bitcoin.bpi.USD.code}";
+
+
+            }
+        }
+
+        public static BitcoinRates GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+            HttpWebRequest request=(HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse= request.GetResponse();
+            var webStream= webResponse.GetResponseStream();
+
+            BitcoinRates bitcoin;
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoin = JsonConvert.DeserializeObject<BitcoinRates>(response);
+
+            }
+            return bitcoin;
+        }
+
+        private void Currency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ammountOfCoin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
